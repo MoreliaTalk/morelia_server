576fba2e6b428faad4b966556c09a81a5c3bfc73
NekrodNIK <NP17210@yandex.ru>
2020-07-02 11:05:09 +0600

    Update app.py

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ async def websocket_endpoint(websocket: WebSocket):
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
