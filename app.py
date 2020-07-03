8342e529f030cd32701deb9a4a9cf1425f186d7f
NekrodNIK <NP17210@yandex.ru>
2020-07-03 14:39:01 +0600

    add SQL database
    add SQL database(ALPHA)

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from time import time
 from datetime import datetime
 
 import uvicorn
+from database import main as db
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi import WebSocket
@@ -13,10 +14,6 @@ app = FastAPI()
 
 templates = Jinja2Templates(directory='templates')
 
-list_userdata = {"Jack": "123"}
-# Сохраняем список сообщенией,
-# TODO: Сохраняем в базу данных
-list_message = []
 # Сохраняем сессии клиентов
 # TODO:   При отключении нужно реализовать их удаление  подумать как их хранить
 clients = []
@@ -39,14 +36,9 @@ def status_page(request: Request):
                                        'stats': stats})
 
 
-# сохраняем сообщение в базу
-def save_message(mes):
-    list_message.append(mes)
-
-
 # Отправляем все сообщения чата
 async def get_all_messages(newclient):
-    for message in list_message:
+    for message in db.get_messages():
         await newclient.send_json(message)
 
 
@@ -58,15 +50,14 @@ async def send_message(mes):
 
 # TODO: надо релизовать регистрацию
 async def reg_user(data):
-    if data["username"] in list_userdata:
-        if data["password"] == list_userdata[data["username"]]:
+    if (password := db.get_userdata(data["username"])) != None:
+        if data["password"] == password:
             return "true"
         else:
             return "false"
     else:
-        list_userdata[data["username"]] = data["password"]
+        db.save_userdata(data["username"], data["password"])
         return "newreg"
-
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
@@ -84,7 +75,7 @@ async def websocket_endpoint(websocket: WebSocket):
                         "text": data["text"],
                         "timestamp": time()
                         }
-                save_message(message)
+                db.save_message(message)
                 print(message)
                 await send_message(message)
             elif data["mode"] == "reg":
