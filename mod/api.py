c7ad7dabb3d0736e9182026b003b5d51ca65e3e8
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-07-03 19:29:11 +0300

    Update api.py
    minor changes

--- mod/api.py
+++ mod/api.py
@@ -1,8 +1,5 @@
 import json
 from time import time
-from hashlib import blake2b
-from os import urandom
-from hmac import compare_digest
 from typing import Any
 from typing import Optional
 
@@ -14,67 +11,20 @@ API_VERSION: bool = 1.0
 
 
 class PasswordValidate(BaseModel):
+    """Checks data type and converts it to 'bytes' type
+
+    Args:
+        password (str, int, bytes): Required parameter
+        salt (str, int, bytes): optional parameter
+        key (str, int, bytes): optional parameter
+        hash_password (str, int, bytes): optional parameter
+    """
+    class Config:
+        title = 'Password object'
     password: bytes
-    salt: bytes = None
-    key: bytes = None
-    hash_password: bytes = None
-
-
-def password_hash(password: str, salt: bytes = None,
-                  key: bytes = None) -> dict:
-    """Function generates a a password hash.
-
-    Args:
-        password (str): password
-        salt (bytes, optional): Salt. Defaults to None.
-        key (bytes, optional): Additional argument. Defaults to None.
-        If the value of the 'key' parameter is 'None' then the function
-        will set it to 'byte-like object' equal to an empty string.
-
-    Returns:
-        dict: Returns the dictionary with the original parameters passed
-        to the function and password hash.
-
-    """
-    valid = PasswordValidate(password=password, salt=salt, key=key)
-    size: int = 32
-    if valid.salt is None:
-        salt = urandom(16)
-    if valid.key is None:
-        key = b''
-    hash_password = blake2b(valid.password, digest_size=size, key=key, salt=salt)
-    result = {
-        'password': valid.password,
-        'hash_password': hash_password.hexdigest().encode('utf-8'),
-        'salt': valid.salt,
-        'key': valid.key
-    }
-    return result
-
-
-def check_password(hash_password: bytes, password: str,
-                   salt: bytes, key: bytes = None) -> bool:
-    """The function checks the password hash and original password.
-
-    Args:
-        hash_password (bytes): Hash password
-        password (str): Password
-        salt (bytes): Salt
-        key (bytes, optional): Additional argument.
-        If the hash password was obtained using an additional key "key",
-        that key must be entered. Defaults to None.
-
-    Returns:
-        bool: True or False
-
-    """
-    data = password.encode('utf-8')
-    size: int = 32
-    if key is None:
-        key = b''
-    result = blake2b(data, digest_size=size, key=key, salt=salt)
-    good_password = result.hexdigest()
-    return compare_digest(hash_password, good_password.encode('utf-8'))
+    salt: Optional[bytes] = None
+    key: Optional[bytes] = None
+    hash_password: Optional[bytes] = None
 
 
 dict_json = {
@@ -138,10 +88,7 @@ dict_json = {
     'meta': None
     }
 
-print('Size dict=', asizeof.asizeof(dict_json))
 encode_json = json.JSONEncoder().encode(dict_json)
-print('Size json=', asizeof.asizeof(encode_json))
-print('Encode=', encode_json)
 
 
 class APIEditedMessage(BaseModel):
@@ -164,6 +111,7 @@ class APIFromChat(BaseModel):
     class Config:
         title = 'Information from chat id'
     id: int
+
 
 class APIChat(BaseModel):
     class Config:
@@ -251,9 +199,39 @@ def response(obj):
         validate = JsonAPI.parse_raw(obj)
     except ValidationError as error:
         return error.json()
-    if validate.dict()['type'] == 'user_info':
+    if validate.dict()['type'] == 'all_messages':
         try:
-            result = APIErrors(id=1, time=time(), status='OK', code=200, detail='Hello')
+            result = APIErrors(id=validate.dict()['data']['user']['id'], time=time(), status='OK', code=200, detail='Hello')
+        except ValidationError as error:
+            result = error
+    elif validate.dict()['type'] == 'auth':
+        try:
+            result = APIErrors(id=validate.dict()['data']['user']['id'], time=time(), status='OK', code=200, detail='Hello')
+        except ValidationError as error:
+            result = error
+    elif validate.dict()['type'] == 'all_chat':
+        try:
+            result = APIErrors(id=validate.dict()['data']['user']['id'], time=time(), status='OK', code=200, detail='Hello')
+        except ValidationError as error:
+            result = error
+    elif validate.dict()['type'] == 'reg_user':
+        try:
+            result = APIErrors(id=validate.dict()['data']['user']['id'], time=time(), status='OK', code=200, detail='Hello')
+        except ValidationError as error:
+            result = error
+    elif validate.dict()['type'] == 'user_info':
+        try:
+            result = APIErrors(id=validate.dict()['data']['user']['id'], time=time(), status='OK', code=200, detail='Hello')
+        except ValidationError as error:
+            result = error
+    elif validate.dict()['type'] == 'send_message':
+        try:
+            result = APIErrors(id=validate.dict()['data']['user']['id'], time=time(), status='OK', code=200, detail='Hello')
+        except ValidationError as error:
+            result = error
+    elif validate.dict()['type'] == 'get_update':
+        try:
+            result = APIErrors(id=validate.dict()['data']['user']['id'], time=time(), status='OK', code=200, detail='Hello')
         except ValidationError as error:
             result = error
     else:
@@ -265,4 +243,4 @@ def response(obj):
 
 
 print('Response=', response(encode_json))
-#print('Shema=', JsonAPI.schema_json(indent=2))
+print('Shema=', JsonAPI.schema_json(indent=2))
