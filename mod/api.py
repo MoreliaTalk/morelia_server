0e34899685489b4bfa67a68f1368f7c77713d8db
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-07-03 11:09:49 +0300

    describes the basic version of the data model that will validate 'pydantic'.

--- mod/api.py
+++ mod/api.py
@@ -74,40 +74,174 @@ def check_password(hash_password: bytes, password: str,
     return compare_digest(hash_password, good_password.encode('utf-8'))
 
 
+
 dict_json = {
-    'type_': 'function',
-    'data': None,
-    'name': {
-        'user': 'user'
-    },
-    'meta': True
-}
+            'type': 'user_info',
+            'data': {
+                'time': time(),
+                'chat': {
+                    'id': 1254,
+                    'time': time(),
+                    'type': 'chat',
+                    'title': 'Name Chat',
+                    'info': 'Info about this chat'
+                    },
+                'message': {
+                    'id': 1,
+                    'text': 'some text...',
+                    'from_user': {'user': 'User'},
+                    'time': time(),
+                    'chat': {},
+                    'file': {
+                        'picture': 'jkfikdkdsd',
+                        'video': 'sdfsdfsdf',
+                        'audio': '\fgf\sdfsdf\sdf',
+                        'document': ''
+                        },
+                    'emoji': 'sfdfsdfsdf',
+                    'edited': {
+                        'time': time(),
+                        'status': True
+                        },
+                    'reply_to': None
+                    },
+                'user': {
+                    'id': 5855,
+                    'login': 'username1',
+                    'password': 'lksdjflksjfsd',
+                    'username': 'Vasya',
+                    'is_bot': True,
+                    'auth_id': '464645646464',
+                    'email': 'stepan.skrjabin@gmail.com',
+                    'avatar': '\fff\ddd\ddd',
+                    'bio': 'My bio'
+                    },
+                'meta': None
+                },
+            'errors': {
+                'id': 25665546,
+                'time': time(),
+                'status': 'OK',
+                'code': 200,
+                'detail': 'successfully'
+                },
+            'jsonapi': {
+                'version': 1.0
+                    },
+            'meta': None
+        }
 
 encode_json = json.JSONEncoder().encode(dict_json)
-print(encode_json)
+print('Encode=', encode_json)
 
 
-class ValidateJSONrequest(BaseModel):
-    type_: str
-    data: str = None
-    name: dict
-    meta: bool
+class APIEditedMessage(BaseModel):
+    class Config:
+        title = 'Time of editing the message'
+    time: int = None
+    status: bool = None
 
 
-class ValidateJSONresponse(BaseModel):
-    status: str
-    code: int
+class APIFile(BaseModel):
+    class Config:
+        title = 'Files attached to the message'
+    picture: bytes = None
+    video: bytes = None
+    audio: bytes = None
+    document: bytes = None
+
+
+class APIChat(BaseModel):
+    class Config:
+        title = 'List of chat rooms with their description and type'
+    id: int = None
+    time: int = None
+    type: str = None
+    title: str = None
+    info: str = None
+
+class APIUser(BaseModel):
+    class Config:
+        title = 'User information'
+    id: int = None
+    login: str = None
+    password: str = None
+    username: str = None
+    is_bot: Optional[bool] = None
+    auth_id: int = None
+    email: EmailStr = None
+    avatar: bytes = None
+    bio: str = None
+
+
+class APIMessage(BaseModel):
+    class Config:
+        title = 'Message options'
+    id: int = None
+    text: str = None
+    from_user: APIUser
+    time: int = None
+    chat: APIChat = None
+    file: APIFile = None
+    emoji: bytes = None
+    edited: APIEditedMessage = None
+    # Заглушка, потом удалить.
+    reply_to: Optional[Any] = None
+
+
+class APIData(BaseModel):
+    class Config:
+        title = 'The main object with the data. There is a reserved \'meta\' field.'
+    time: int
+    chat: APIChat = None
+    message: APIMessage = None
+    user: APIUser
+    meta: Any = None
+
+
+class APIErrors(BaseModel):
+    class Config:
+        title = 'Error information and statuses of request processing'
+    id: Optional[int] = None
+    time: Optional[int] = None
+    status: Optional[int] = None
+    code: Optional[int] = None
+    detail: Optional[int] = None
+
+
+class APIVersion(BaseModel):
+    class Config:
+        title = 'Protocol version'
+    version: float
+
+
+class JsonAPI(BaseModel):
+    class Config:
+        title = 'MoreliaTalk protocol version 1'
+    type: str
+    data: Optional[APIData] = None
+    errors: APIErrors = None
+    jsonapi: APIVersion
+    meta: Any = None
 
 
 def response(obj):
-    decoded_json = json.JSONDecoder().decode(obj)
-    valid_json = ValidateJSONrequest.parse_obj(decoded_json)
-    if valid_json.dict()['type_'] == 'function':
-        result = ValidateJSONresponse(status='OK', code=200)
+    try:
+        valid_json = JsonAPI.parse_raw(obj)
+    except ValidationError as error:
+        return error.json()
+    if valid_json.dict()['type'] == 'user_info':
+        try:
+            result = APIErrors(status='OK', code=200)
+        except ValidationError as error:
+            result = error
     else:
-        result = ValidateJSONresponse(status='ERROR', code=400)
-    encoded_json = json.JSONEncoder().encode(result.dict())
-    return encoded_json
+        try:
+            result = APIErrors(status='ERROR', code=400)
+        except ValidationError as error:
+            result = error
+    return result.json()
+    
+print('Response=', response(encode_json))
+#print('Shema=', JsonAPI.schema_json(indent=2))
 
-
-print(response(encode_json))
