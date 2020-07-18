6cae4bfab9bb6a48cffeaf457da86779c85a4982
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-07-18 18:01:14 +0300

    update database/main
    the SQLite database module has been updated

--- database/main.py
+++ database/main.py
@@ -1,29 +1,41 @@
 import sqlite3
-conn = sqlite3.connect('database/database.db', check_same_thread = False)
+
+
+conn = sqlite3.connect('database/database.db',
+                       check_same_thread=False)
 cursor = conn.cursor()
-def save_userdata(username,password):
-    userdata = [username,password]
+
+
+def save_userdata(username, password):
+    userdata = [username, password]
     cursor.execute("INSERT INTO Userdata VALUES(?,?)", userdata)
     conn.commit()
+
+
 def get_userdata(username):
     cursor.execute('SELECT password FROM Userdata WHERE username=?',[username])
     password = cursor.fetchone()
-    if password != None:
+    if password is None:
         return password[0]
     else:
         return None
-def save_message(mes):
-    mesdata = [mes["username"],mes["text"],mes["timestamp"]]
-    cursor.execute("INSERT INTO messages VALUES(?,?,?)", mesdata)
+
+
+def save_message(message):
+    data = [message["username"],
+            message["text"],
+            message["timestamp"]]
+    cursor.execute("INSERT INTO messages VALUES(?,?,?)", data)
     conn.commit()
+
+
 def get_messages():
     messages = []
     for data in cursor.execute('SELECT * FROM messages'):
         messages.append({
-                        "mode": "message",
-                        "username": data[0],
-                        "text": data[1],
-                        "timestamp": data[2]
+                    "mode": "message",
+                    "username": data[0],
+                    "text": data[1],
+                    "timestamp": data[2]
                         })
     return messages
-    
