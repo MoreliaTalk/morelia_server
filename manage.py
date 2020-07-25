851fd447f0bba79a7c710294b76828508ed50016
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-07-25 20:51:17 +0300

    The internal workflow of manage.py has been changed
    Added automatic creation and deletion of database tables after loading models.py.

--- manage.py
+++ manage.py
@@ -1,6 +1,9 @@
-import sqlobject as orm
+import sys
 import time
 import click
+import inspect
+
+import sqlobject as orm
 
 from mod import config
 from mod import models
@@ -10,28 +13,25 @@ connection = orm.connectionForURI(config.LOCAL_SQLITE)
 orm.sqlhub.processConnection = connection
 
 
+classes = [cls_name for cls_name, cls_obj in inspect.getmembers(sys.modules['mod.models']) if inspect.isclass(cls_obj)]
+
+
 @click.command()
 @click.option('--table', default='create', help='Create or delete all table \
     whit all data. The operation, by default, creates all tables')
 def main(table):
     if table == "create":
         start_time = time.process_time()
-        models.User.createTable(ifNotExists=True)
-        models.Flow.createTable(ifNotExists=True)
-        models.Message.createTable(ifNotExists=True)
-        models.Errors.createTable(ifNotExists=True)
+        for item in classes:
+            class_ = getattr(models, item)
+            class_.createTable(ifNotExists=True)
         print(f"Table is createt at: {time.process_time() - start_time} sec.")
         return
     elif table == "delete":
         start_time = time.process_time()
-        models.User.dropTable(
-            ifExists=True, dropJoinTables=True, cascade=True)
-        models.Flow.dropTable(
-            ifExists=True, dropJoinTables=True, cascade=True)
-        models.Message.dropTable(
-            ifExists=True, dropJoinTables=True, cascade=True)
-        models.Errors.dropTable(
-            ifExists=True, dropJoinTables=True, cascade=True)
+        for item in classes:
+            class_ = getattr(models, item)
+            class_.dropTable(ifExists=True, dropJoinTables=True, cascade=True)
         print(f"Table is deleted at: {time.process_time() - start_time} sec.")
         return
     else:
