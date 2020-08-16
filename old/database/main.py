32b71b8bd94185376612b137e01051c552d49020
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-08-16 19:04:43 +0300

    Update main.py
    line break

--- old/database/main.py
+++ old/database/main.py
@@ -8,12 +8,17 @@ cursor = conn.cursor()
 
 def save_userdata(username, password) -> None:
     userdata = [username, password]
-    cursor.execute("INSERT INTO Userdata VALUES(?,?)", userdata)
+    cursor.execute("INSERT INTO Userdata \
+                   VALUES(?,?)",
+                   userdata)
     conn.commit()
 
 
 def get_userdata(username) -> None:
-    cursor.execute('SELECT password FROM Userdata WHERE username=?',[username])
+    cursor.execute('SELECT password \
+                   FROM Userdata \
+                   WHERE username=?',
+                   [username])
     password = cursor.fetchone()
     if password is None:
         return password[0]
@@ -25,17 +30,18 @@ def save_message(message) -> None:
     data = [message["username"],
             message["text"],
             message["timestamp"]]
-    cursor.execute("INSERT INTO messages VALUES(?,?,?)", data)
+    cursor.execute("INSERT INTO messages \
+                   VALUES(?,?,?)",
+                   data)
     conn.commit()
 
 
 def get_messages() -> list:
     messages = []
-    for data in cursor.execute('SELECT * FROM messages'):
-        messages.append({
-                    "mode": "message",
-                    "username": data[0],
-                    "text": data[1],
-                    "timestamp": data[2]
-                        })
+    for data in cursor.execute('SELECT * \
+                               FROM messages'):
+        messages.append({"mode": "message",
+                         "username": data[0],
+                         "text": data[1],
+                         "timestamp": data[2]})
     return messages
