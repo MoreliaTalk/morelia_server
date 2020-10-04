63aaaf4d2596bb7b6b186ae81920df619f99d401
Stepan Skriabin <stepan.skrjabin@gmail.com>
2020-10-05 00:09:11 +0300

    FIX: import server.py
    and minor changes

--- debug_server.py
+++ debug_server.py
@@ -1,9 +1,11 @@
 import uvicorn
 
+from . import server
 
 
 if __name__ == "__main__":
-    uvicorn.run(host="0.0.0.0",
+    uvicorn.run(server,
+                host="0.0.0.0",
                 port=8000,
                 http="h11",
                 ws="websocket",
