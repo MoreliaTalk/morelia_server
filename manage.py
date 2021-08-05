3108611137ca4a1406ef490a69ba6d8c41de6674
Stepan Skriabin <stepan.skrjabin@gmail.com>
2021-08-05 20:10:49 +0300

    feat: option to create a group was added, creation of tables and database are separated into different options
    - DB connection moved to main() function

--- manage.py
+++ manage.py
@@ -1,55 +1,87 @@
 import sys
 import time
-import click
 import inspect
+from uuid import uuid4
 
 import sqlobject as orm
+import click
 
 from mod import config
 from mod import models
 
-# Connect to database
-connection = orm.connectionForURI(config.LOCAL_POSTGRESQL)
-orm.sqlhub.processConnection = connection
-
-# looking for all Classes listed in models.py
-classes = [cls_name for cls_name, cls_obj
-           in inspect.getmembers(sys.modules['mod.models'])
-           if inspect.isclass(cls_obj)]
-
 
 @click.command()
-@click.option('--table', default='create', help='Create or delete all table \
-    whit all data. Оperation, by default, creates all tables')
-def main(table):
-    if table == "create":
+@click.option("--db",
+              type=click.Choice(["create", "delete"]),
+              help='Create or delete all table '
+              'with all data. Оperation, '
+              'by default, creates all tables')
+@click.option('--table',
+              type=click.Choice(["superuser", "flow"]),
+              help='Creating records in the database.'
+              'You can create a superuser or flow type "group".')
+def main(db, table):
+    # Connect to database
+    connection = orm.connectionForURI(config.LOCAL_SQLITE)
+    orm.sqlhub.processConnection = connection
+
+    # looking for all Classes listed in models.py
+    classes = [cls_name for cls_name, cls_obj
+               in inspect.getmembers(sys.modules['mod.models'])
+               if inspect.isclass(cls_obj)]
+
+    if db == "create":
         start_time = time.process_time()
         for item in classes:
             # Create tables in database for each class
             # that is located in models module
             class_ = getattr(models, item)
             class_.createTable(ifNotExists=True)
-        return print(f"Table is createt at: \
-                     {time.process_time() - start_time} sec.")
-    if table == "superuser":
-        models.User(uuid=123456,
-                    login="login",
-                    password="password",
-                    hashPassword="8b915f2f0b0d0ccf27854dd708524d0b\
-                                  5a91bdcd3775c6d3335f63d015a43ce1",
-                    username="superuser",
-                    salt=b"salt",
-                    key=b"key")
-        return print("Create user")
-    elif table == "delete":
+        click.echo(f'Table is createt at: '
+                   f'{time.process_time() - start_time} sec.')
+    elif db == "delete":
         start_time = time.process_time()
         for item in classes:
             class_ = getattr(models, item)
             class_.dropTable(ifExists=True, dropJoinTables=True, cascade=True)
-        return print(f"Table is deleted at: \
-                     {time.process_time() - start_time} sec.")
+        click.echo(f'Table is deleted at: '
+                   f'{time.process_time() - start_time} sec.')
+    elif table == "superuser":
+        hash_password = "8b915f2f0b0d0ccf27854dd708524d0b5a91bdcd3775c6d3335f63d015a43ce1"
+        try:
+            models.UserConfig(uuid=str(123456789),
+                              login="login",
+                              password="password",
+                              hashPassword=hash_password,
+                              username="superuser",
+                              salt=b"salt",
+                              key=b"key")
+            click.echo("Superuser created")
+        except orm.dberrors.OperationalError as error:
+            click.echo(f'Failed to create a user. Error text: {error}')
+    elif table == "flow":
+        user_uuid = str(123456789)
+        hash_password = "8b915f2f0b0d0ccf27854dd708524d0b5a91bdcd3775c6d3335f63d015a43ce1"
+        try:
+            new_user = models.UserConfig(uuid=user_uuid,
+                                         login="login",
+                                         password="password",
+                                         hashPassword=hash_password,
+                                         username="superuser",
+                                         salt=b"salt",
+                                         key=b"key")
+            new_flow = models.Flow(uuid=str(uuid4().hex),
+                                   timeCreated=int(time()),
+                                   flowType="group",
+                                   title="Test",
+                                   info="Test flow",
+                                   owner=user_uuid)
+            new_flow.addUserConfig(new_user)
+            click.echo("Flow created")
+        except orm.dberrors.OperationalError as error:
+            click.echo(f'Failed to create a flow. Error text: {error}')
     else:
-        return print("ERROR. Function \'--table\' did not work")
+        click.echo("ERRROOOOR")
 
 
 if __name__ == "__main__":
