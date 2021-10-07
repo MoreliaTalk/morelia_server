44c8c3429c19c1f58955ecfce75bcdcf0a202985
Stepan Skriabin <stepan.skrjabin@gmail.com>
2021-10-07 23:33:50 +0300

    feat(error): error dictionary is replaced by the function
    new function "check_error_pattern" checking the error status against the class HTTPStatus or ServerStatus

--- mod/error.py
+++ mod/error.py
@@ -17,86 +17,38 @@
     You should have received a copy of the GNU Lesser General Public License
     along with Morelia Server. If not, see <https://www.gnu.org/licenses/>.
 """
+from http import HTTPStatus
+from enum import Enum, IntEnum
 
-DICT: dict = {
-            200: {
-                'status': 'OK',
-                'detail': 'successfully'
-                },
-            201: {
-                'status': 'Created',
-                'detail': 'Created'
-                },
-            202: {
-                'status': 'Accepted',
-                'detail': 'Accepted'
-                },
-            206: {
-                'status': "Partial Content",
-                'detail': "Partial Content"
-                },
-            400: {
-                'status': 'Bad Request',
-                'detail': 'Bad Request'
-                },
-            401: {
-                'status': 'Unauthorized',
-                'detail': 'Unauthorized'
-                },
-            403: {
-                'status': 'Forbidden',
-                'detail': 'Forbidden'
-                },
-            404: {
-                'status': 'Not Found',
-                'detail': 'Not Found'
-                },
-            405: {
-                'status': 'Method Not Allowed',
-                'detail': 'Method Not Allowed'
-                },
-            408: {
-                'status': 'Request Timeout',
-                'detail': 'Request Timeout'
-                },
-            409: {
-                'status': 'Conflict',
-                'detail': 'Such user (flow) is already on the server.'
-                },
-            415: {
-                'status': 'Unsupported Media Type',
-                'detail': 'Unsupported Media Type'
-                },
-            417: {
-                'status': 'Expectation Failed',
-                'detail': 'Expectation Failed'
-                },
-            426: {
-                'status': 'Upgrade Required',
-                'detail': 'Upgrade Required'
-                },
-            429: {
-                'status': 'Too Many Requests',
-                'detail': 'Too Many Requests'
-                },
-            499: {
-                'status': 'Client Closed Request',
-                'detail': 'Client Closed Request'
-                },
-            500: {
-                'status': 'Internal Server Error',
-                'detail': 'Internal Server Error'
-                },
-            503: {
-                'status': 'Service Unavailable',
-                'detail': 'Service Unavailable'
-                },
-            520: {
-                'status': 'Unknown Error',
-                'detail': 'Unknown Error'
-            },
-            526: {
-                'status': 'Invalid SSL Certificate',
-                'detail': 'Invalid SSL Certificate'
-                },
-            }
+
+class ServerStatus(IntEnum):
+    """Server status code and reason phrases
+
+    Args:
+        enum ([type]): [description]
+    """
+    def __new__(cls, value, phrase, description=''):
+        obj = int.__new__(cls, value)
+        obj._value_ = value
+        obj.phrase = phrase
+        obj.description = description
+        return obj
+
+    CLIENT_CLOSED_REQUEST = 499, 'Client Closed Request', 'Client Closed Request'
+    UNKNOWN_ERROR = 520, 'Unknown Error1', 'Unknown Error2'
+    INVALID_SSL_CERTIFICATE = 526, 'Invalid SSL Certificate', 'Invalid SSL Certificate'
+
+
+def check_error_pattern(status: str) -> Enum:
+    try:
+        code = getattr(HTTPStatus, status).value
+    except AttributeError:
+        code = getattr(ServerStatus, status).value
+        status = getattr(ServerStatus, status).phrase
+        description = getattr(ServerStatus, status).description
+    else:
+        status = getattr(HTTPStatus, status).phrase
+        description = getattr(HTTPStatus, status).description
+    return Enum("Error", {"code": code,
+                          "status": status,
+                          "detail": description})
