6c890ef9783c980799af8815638868eb6165357d
Stepan Skriabin <stepan.skrjabin@gmail.com>
2021-10-23 13:24:07 +0300

    feat(test): added new test
    - checking false status in password_check
    - checking get_key and get_salt

--- tests/test_lib.py
+++ tests/test_lib.py
@@ -57,11 +57,11 @@ class TestHash(unittest.TestCase):
         del self.key
         del self.uuid
 
-    def test_check_str_in_result(self):
+    def test_str_in_result(self):
         self.assertIsInstance(self.generator.password_hash(),
                               str)
 
-    def test_check_space_password(self):
+    def test_space_password(self):
         self.password = ' '
         self.generator = lib.Hash(self.password,
                                   self.uuid,
@@ -69,7 +69,7 @@ class TestHash(unittest.TestCase):
         self.assertIsInstance(self.generator.password_hash(),
                               str)
 
-    def test_check_256_symbols_password(self):
+    def test_256_symbols_password(self):
         self.password = '8b915f2f0b0d0ccf27854dd708524d0b\
                          5a91bdcd3775c6d3335f63d015a43ce1'
         self.generator = lib.Hash(self.password,
@@ -78,7 +78,7 @@ class TestHash(unittest.TestCase):
         self.assertIsInstance(self.generator.password_hash(),
                               str)
 
-    def test_check_wrong_password_type(self):
+    def test_wrong_password_type(self):
         self.password = 123456789
         self.assertRaises(AttributeError,
                           lib.Hash,
@@ -95,7 +95,7 @@ class TestHash(unittest.TestCase):
         self.assertRaises(TypeError,
                           generator.check_password)
 
-    def test_check_password(self):
+    def test_check_password_is_true(self):
         self.new_generator = lib.Hash(self.password,
                                       self.uuid,
                                       self.salt,
@@ -103,9 +103,23 @@ class TestHash(unittest.TestCase):
                                       hash_password=self.hash_password)
         self.assertTrue(self.new_generator.check_password())
 
+    def test_check_password_is_false(self):
+        self.new_generator = lib.Hash(self.password,
+                                      self.uuid,
+                                      self.salt,
+                                      self.key,
+                                      hash_password=None)
+        self.assertFalse(self.new_generator.check_password())
+
     def test_check_auth_id(self):
         self.assertIsInstance(self.generator.auth_id(),
                               str)
+
+    def test_check_get_salt_and_get_key(self):
+        self.assertEqual(self.generator.get_salt, self.salt)
+        self.assertEqual(self.generator.get_key, self.key)
+        self.assertIsInstance(self.generator.get_salt, bytes)
+        self.assertIsInstance(self.generator.get_key, bytes)
 
 
 if __name__ == "__main__":
