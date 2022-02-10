0f265bf0e13871c7653f63f4502542a1facccead
NekrodNIK <NP17210@yandex.ru>
2022-02-10 21:04:47 +0500

    fix test

--- tests/test_server.py
+++ tests/test_server.py
@@ -2,12 +2,16 @@ import json
 import logging
 
 from starlette.testclient import TestClient, WebSocketTestSession
+from starlette.websockets import WebSocketDisconnect
 
 from server import app
 import unittest
 
 
 class TestWebsocket(unittest.TestCase):
+    class DisconnectErr(Exception):
+        pass
+
     @classmethod
     def setUpClass(cls):
         cls.ws_client = TestClient(app)
@@ -16,21 +20,44 @@ class TestWebsocket(unittest.TestCase):
         with self.ws_client.websocket_connect("/ws") as connection:
             self.assertIsInstance(connection, WebSocketTestSession)
 
+    def test_not_websocket_endpoint_connect(self):
+        with self.assertRaises(WebSocketDisconnect):
+            with self.ws_client.websocket_connect("/no-ws"):
+                pass
+
     def test_normal_disconnect(self):
         with self.ws_client.websocket_connect("/ws") as connection:
             connection.close()
 
     def test_not_normal_disconnect(self):
-        with self.ws_client.websocket_connect("/ws") as connection:
-            connection.close(1006)
+        with self.assertRaises(self.DisconnectErr):
+            with self.ws_client.websocket_connect("/ws") as connection:
+                def callback():
+                    raise self.DisconnectErr
+
+                connection.exit_stack.callback(
+                    callback
+                )
+                connection.close(1006)
 
     def test_send_normal_message(self):
         with self.ws_client.websocket_connect("/ws") as connection:
-            connection.send_json(json.dumps({}))
+            connection.send_json({
+                "type": "ping-pong",
+                "data": {"user": [{}]},
+                "jsonapi": {
+                    "version": "1.0",
+                    "revision": "17"
+                    },
+                "meta": None
+            })
+            self.assertIsNotNone(json.loads(connection.receive_bytes()))
 
     def test_send_incorrect_message(self):
-        with self.ws_client.websocket_connect("/ws") as connection:
-            connection.send_text("hello error!")
+        with self.assertRaises(WebSocketDisconnect):
+            with self.ws_client.websocket_connect("/ws") as connection:
+                connection.send_text("hello error!")
+                connection.receive_bytes()
 
 
 class TestMainPage(unittest.TestCase):
