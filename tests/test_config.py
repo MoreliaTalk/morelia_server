1d688537f88fffbb29958a787c4346516fbda5f5
stepanskryabin <stepan.skrjabin@gmail.com>
2022-02-19 00:07:06 +0300

    feat(tests): added new fixtures and new tests for config module

--- tests/test_config.py
+++ tests/test_config.py
@@ -18,14 +18,21 @@
     You should have received a copy of the GNU Lesser General Public License
     along with Morelia Server. If not, see <https://www.gnu.org/licenses/>.
 """
+
 import os
+import stat
 import unittest
 from pathlib import PurePath
 from loguru import logger
+from pydantic import ValidationError
 
 from mod.config.config import ConfigHandler
+from mod.config.config import AccessConfigError
+from mod.config.config import BackUpConfigError
+from mod.config.config import RebuildConfigError
 from mod.config.config import ConfigModel
-from mod.lib import rebuild_config
+from mod.config.config import NameConfigError
+from mod.config.config import CopyConfigError
 
 
 class TestConfigHandler(unittest.TestCase):
@@ -34,12 +41,24 @@ class TestConfigHandler(unittest.TestCase):
         logger.remove()
 
     def setUp(self) -> None:
+        self.example_config = 'example_config.ini'
         self.config_name = 'config.ini'
+        self.non_write_config = 'non_write_config.ini'
+        self.fixtures = 'tests/fixtures'
         self.test = ConfigHandler()
 
     def tearDown(self) -> None:
+        del self.example_config
+        del self.non_write_config
         del self.config_name
+        del self.fixtures
         del self.test
+
+    def test_set_configparser(self):
+        self.test._set_configparser(name=self.config_name,
+                                    directory=None)
+        result = self.test._config.sections()
+        self.assertEqual(result[0], "DATABASE")
 
     def test__str__(self):
         self.assertRegex(str(self.test),
@@ -52,17 +71,37 @@ class TestConfigHandler(unittest.TestCase):
         self.assertRegex(result,
                          self.config_name)
 
-    def test_set_configparser(self):
-        self.test._set_configparser(self.config_name,
-                                    None)
-        result = self.test.config.sections()
-        self.assertEqual(result[0], "DATABASE")
+    def test_copy_string(self):
+        source = self.test._search_config(name=self.example_config,
+                                          directory=None)
+        destination = self.test._search_config(name=self.config_name,
+                                               directory=self.fixtures)
+        result = self.test._copy_string(src=source,
+                                        dst=destination)
+        self.assertEqual(result, "Copied string success")
 
-    def test_search_file(self):
+    def test_wrong_copy_string(self):
+        source = self.test._search_config(name=self.example_config,
+                                          directory=None)
+        destination = self.test._search_config(name=self.non_write_config,
+                                               directory=self.fixtures)
+        self.assertRaises(CopyConfigError,
+                          self.test._copy_string,
+                          src=source,
+                          dst=destination)
+
+    def test_search_config(self):
         full_path = self.test._search_config(self.config_name,
                                              None)
         result = PurePath(full_path)
-        self.assertEqual(result.parts[-1], 'config.ini')
+        self.assertIsInstance(result, (str, PurePath))
+        self.assertEqual(result.parts[-1], self.config_name)
+
+    def test_wrong_search_config(self):
+        self.assertRaises(NameConfigError,
+                          self.test._set_configparser,
+                          name=self.example_config,
+                          directory=self.fixtures)
 
     def test_backup_file(self):
         result = self.test._backup_config_file()
@@ -73,6 +112,17 @@ class TestConfigHandler(unittest.TestCase):
         # delete created backup file
         os.remove(result[1])
 
+    def test_wrong_backup_file(self):
+        self.test.root_directory = self.fixtures
+        destination = self.test._search_config(name=self.config_name,
+                                               directory=self.fixtures)
+        # Change mode to not read, not write, not execute to all users
+        os.chmod(destination, stat.S_ENFMT)
+        self.assertRaises(BackUpConfigError,
+                          self.test._backup_config_file)
+        # Change mode to read, write, execute to all users
+        os.chmod(destination, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+
     def test_validate(self):
         valid = self.test._validate()
         result = valid.dict()
@@ -82,16 +132,30 @@ class TestConfigHandler(unittest.TestCase):
         self.assertEqual(result['uri'], 'sqlite:db_sqlite.db')
         self.assertEqual(result['folder'], 'templates')
 
+    def test_rebuild_config(self):
+        result = self.test._rebuild_config()
+        self.assertEqual(result, 'Config rebuild success')
+
+    def test_wrong_rebuild_config(self):
+        destination = self.test._search_config(name=self.config_name,
+                                               directory=self.fixtures)
+        # Change mode to not read, not write, not execute to all users
+        os.chmod(destination, stat.S_ENFMT)
+        self.assertRaises(RebuildConfigError,
+                          self.test._rebuild_config)
+        # Change mode to read, write, execute to all users
+        os.chmod(destination, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+
     def test_get_root_directory(self):
         directory = self.test.root_directory
         self.assertEqual(directory, None)
 
     def test_set_root_directory(self):
-        new_root_directory = 'tests'
-        self.test.root_directory = new_root_directory
+        self.test.root_directory = self.fixtures
         result = PurePath(self.test.file_path)
-        self.assertEqual(result.parts[-1], "config.ini")
-        self.assertEqual(result.parts[-2], new_root_directory)
+        self.assertEqual(result.parts[-1], self.config_name)
+        self.assertEqual(result.parts[-2], 'fixtures')
+        self.assertEqual(result.parts[-3], 'tests')
 
     def test_get_config_name(self):
         result = self.test.config_name
@@ -108,12 +172,45 @@ class TestConfigHandler(unittest.TestCase):
         self.assertEqual(result.uri, 'sqlite:db_sqlite.db')
         self.assertEqual(result.folder, 'templates')
 
-    def test_write(self):
-        self.test.root_directory = './tests'
+    def test_wrong_read(self):
+        self.test.root_directory = self.fixtures
+        self.test.config_name = 'wrong_config.ini'
+        self.assertRaises(ValidationError,
+                          self.test.read)
+
+    def test_write_without_backup(self):
+        self.test.root_directory = self.fixtures
         result = self.test.write(section='database',
                                  key="url",
                                  value="www.test.ru",
                                  backup=False)
-        self.assertEqual(result, 'Completed')
+        self.assertEqual(result[0], 'Completed')
+        self.assertEqual(result[1], None)
         # rebuild config.ini to tests
-        rebuild_config()
+        self.test._rebuild_config()
+
+    def test_write_with_backup(self):
+        self.test.root_directory = self.fixtures
+        result = self.test.write(section='database',
+                                 key="url",
+                                 value="www.test.ru",
+                                 backup=True)
+        self.assertEqual(result[0], 'Completed')
+        self.assertRegex(PurePath(result[1]).parts[-1],
+                         'config.ini.BAK+')
+        # rebuild config.ini to tests
+        self.test._rebuild_config()
+        # delete created backup file
+        os.remove(result[1])
+
+    def test_write_with_access_error(self):
+        self.test.root_directory = self.fixtures
+        self.test.config_name = self.non_write_config
+        self.assertRaises(AccessConfigError,
+                          self.test.write,
+                          section='database',
+                          key="url",
+                          value="www.test.ru",
+                          backup=False)
+        # rebuild config.ini to tests
+        self.test._rebuild_config()
