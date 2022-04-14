1dafb3c417e13a9f08dec931bcaced57722c02dd
Nekrod <NP17210@yandex.ru>
2022-04-14 17:06:10 +0500

    new batch of type conflict fixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,6 +12,7 @@
 #
 import os
 import sys
+from typing import List
 
 source_path = os.path.split(os.path.abspath("."))
 doc_path = os.path.split(source_path[0])
@@ -61,7 +62,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns: List[str] = []
 
 # -- Options for HTML output -------------------------------------------------
 
