f75242378ae7c862479751008bb42e4cfe36d17a
stepanskryabin <stepan.skrjabin@gmail.com>
2022-05-14 16:27:44 +0300

    fixed(db): fix text message return in DatabaseReadError
    and DatabaseAccessError

--- mod/db/dbhandler.py
+++ mod/db/dbhandler.py
@@ -236,20 +236,20 @@ class DBHandler:
             try:
                 dbquery = db.selectBy(self.connection,
                                       **kwargs).getOne()
-            except (SQLObjectNotFound, SQLObjectIntegrityError):
-                raise DatabaseReadError
+            except (SQLObjectNotFound, SQLObjectIntegrityError) as err:
+                raise DatabaseReadError(err)
             except Exception as err:
-                raise DatabaseAccessError from err
+                raise DatabaseAccessError(err)
             else:
                 return dbquery
         else:
             try:
                 dbquery = db.selectBy(self.connection,
                                       **kwargs)
-            except SQLObjectNotFound:
-                raise DatabaseReadError
+            except SQLObjectNotFound as err:
+                raise DatabaseReadError(err)
             except Exception as err:
-                raise DatabaseAccessError from err
+                raise DatabaseAccessError(err)
             else:
                 return dbquery
 
