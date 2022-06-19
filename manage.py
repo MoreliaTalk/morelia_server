164d1dea7ca7ce3144c8cc2aadc9b820a11b7755
nekrod <NP17210@yandex.ru>
2022-06-19 13:12:14 +0500

    fix

--- manage.py
+++ manage.py
@@ -34,8 +34,8 @@ import uvicorn
 from websockets import client as ws_client
 from websockets import exceptions as ws_exceptions
 
-from mod.config.handler import ConfigHandler
 from mod.config.handler import BackupNotFoundError
+from mod.config.handler import ConfigHandler
 from mod.db.dbhandler import DatabaseAccessError
 from mod.db.dbhandler import DatabaseReadError
 from mod.db.dbhandler import DatabaseWriteError
