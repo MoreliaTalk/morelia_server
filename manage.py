f27f9eedf8d4a408194eef122816121d8ac0caee
nekrod <NP17210@yandex.ru>
2022-12-29 15:08:07 +0500

    add create tables in manage.py

--- manage.py
+++ manage.py
@@ -1,31 +1,46 @@
-from enum import Enum
 import rich
 import typer
 import uvicorn
-import logging as standard_logging
+
+from mod.config.handler import read_config
+from mod.db.dbhandler import DBHandler, DatabaseReadError, DatabaseAccessError, DatabaseWriteError
 
 cli = typer.Typer(help="CLI for management MoreliaServer",
                   no_args_is_help=True)
 
+config_option = read_config()
+
 
 @cli.command()
-def production_server():
-    rich.print("Welcome to cli")
+def create_tables(uri: str = config_option.database.url):
+    database = DBHandler(uri)
+    try:
+        database.create_table()
+    except (DatabaseReadError,
+            DatabaseAccessError,
+            DatabaseWriteError) as err:
+        rich.print(f"The database is unavailable, table not created. {err}")
+    else:
+        rich.print("Tables in db is created.")
 
 
-@cli.command("devserver")
+@cli.command()
 def devserver(host: str = typer.Option("127.0.0.1",
                                        help="Host for running server"),
               port: int = typer.Option(8080,
                                        help="Port for running server"),
-              on_uvicorn_logger: bool = typer.Option(False, help="Activate uvicorn logging")):
-    if on_uvicorn_logger:
-        uvicorn_logger = standard_logging.getLogger("uvicorn")
-        uvicorn_logger.handlers.clear()
+              off_uvicorn_logger: bool = typer.Option(True,
+                                                      "--off-uvicorn-logger",
+                                                      help="Disabled uvicorn logging")):
+    if off_uvicorn_logger:
+        log_level = "critical"
+    else:
+        log_level = "debug"
 
     uvicorn.run("server:app",
                 host=host,
                 port=port,
+                log_level=log_level,
                 debug=True,
                 reload=True)
 
